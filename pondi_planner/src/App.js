8b8e44adf3a2073216eb86187533f9f831f472bc
wimpywarlord <wimpywarlord@gmail.com>
2020-06-27 03:29:05 +0530

    added design

--- pondi_planner/src/App.js
+++ pondi_planner/src/App.js
@@ -1,26 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <BrowserRouter>
+            <div className="App">
+                {/* NAV BAR GOES HERE */}
+                <Switch>
+                    <Route path="/" exact>
+                        <p>sdadas</p>
+                    </Route>
+                    <Route path="/boom" exact>
+                        <p>dasd</p>
+                    </Route>
+                </Switch>
+            </div>
+        </BrowserRouter>
+    );
 }
 
 export default App;
