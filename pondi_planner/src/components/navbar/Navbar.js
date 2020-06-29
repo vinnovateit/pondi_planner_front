71f4ef650ad9d4a6be27e2d77418ea7a89ff41d4
ayushbansalvit <ayush.bansal2019@vitstudent.ac.in>
2020-06-29 10:24:29 +0530

    fixed-navbar-along-with-font

--- pondi_planner/src/components/navbar/Navbar.js
+++ pondi_planner/src/components/navbar/Navbar.js
@@ -3,58 +3,53 @@ import logo from "../../assets/summer.png";
 import "./Navbar.css";
 
 const NavBar = () => {
-    return (
-        <div>
-            <nav
-                style={{ backgroundColor: "rgba(0,0,0,0.0001)" }}
-                className="navbar navbar-expand-lg navbar-dark"
-            >
-                <a className="navbar-brand" href="#">
-                    <img height="25px" width="25px" src={logo} alt="" />
-                </a>
-                <button
-                    className="navbar-toggler"
-                    type="button"
-                    data-toggle="collapse"
-                    data-target="#navbarNav"
-                    aria-controls="navbarNav"
-                    aria-expanded="false"
-                    aria-label="Toggle navigation"
-                >
-                    <span className="navbar-toggler-icon"></span>
-                </button>
-                <div className="collapse navbar-collapse" id="navbarNav">
-                    <ul className="navbar-nav">
-                        <li className="nav-item active">
-                            <a className="nav-link" href="#">
-                                Home <span className="sr-only">(current)</span>
-                            </a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link" href="#">
-                                Book Now
-                            </a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link" href="#">
-                            Explore <sup><span className="badge ">PREMIUM</span></sup>
-                            </a>
-                        </li>
-                        <li className="nav-item">
-                            <a
-                                className="nav-link disabled"
-                                href="#"
-                                tabindex="-1"
-                                aria-disabled="true"
-                            >
-                                Coming Soon
-                            </a>
-                        </li>
-                    </ul>
-                </div>
-            </nav>
+  return (
+    <div>
+      <nav
+        style={{ backgroundColor: "rgba(0,0,0,0.0001)" }}
+        className="navbar navbar-expand-lg navbar-dark"
+      >
+        <a className="navbar-brand" href="#">
+          <img height="62px" width="62px" src={logo} alt="" />
+        </a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav">
+            <li className="nav-item active">
+              <a className="nav-link" href="#">
+                HOME <span className="sr-only">(current)</span>
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link active" href="#">
+                BOOKNOW
+              </a>
+            </li>
+
+            <li className="nav-item">
+              
+              <a className="nav-link active" href="#">
+                EXPLORE
+              </a>
+              <sup>
+                <span className="badge">PREMIUM</span>
+              </sup>
+            </li>
+          </ul>
         </div>
-    );
+      </nav>
+    </div>
+  );
 };
 
 export default NavBar;
